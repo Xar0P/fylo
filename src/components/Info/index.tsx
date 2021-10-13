2de700606f698b2670f361dba37e4e81ff267fd3
Xar0P <pietroricardocres@hotmail.com>
2021-10-13 10:57:38 -0300

    typed info

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -5,30 +5,64 @@ import anyFileImg from '../../assets/icon-any-file.svg';
 
 import { Container, Content, Item } from "./styles";
 
+interface Feature {
+    img: {
+        src: string;
+        alt: string;
+    }
+    title: string;
+    content: string;
+}
+
 export function Info() {
+
+    let features: Feature[] = [
+        {
+            img: {
+                src: accessAnywhereImg,
+                alt: 'Access Anywhere Img',
+            },
+            title: 'Access your files, anywhere',
+            content: 'The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.'
+        },
+        {
+            img: {
+                src: securityImg,
+                alt: 'Security Img',
+            },
+            title: 'Security you can trust',
+            content: '2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.'
+        },
+        {
+            img: {
+                src: collaborationImg,
+                alt: 'Collaboration Img',
+            },
+            title: 'Real-time collaboration',
+            content: 'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.'
+        },
+        {
+            img: {
+                src: anyFileImg,
+                alt: 'any File Img',
+            },
+            title: 'Store any type of file',
+            content: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+        }
+    ]
+
     return (
         <Container>
             <Content>
-                <Item>
-                    <img src={accessAnywhereImg} alt="" />
-                    <h2>Access your files, anywhere</h2>
-                    <p>The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
-                </Item>
-                <Item>
-                    <img src={securityImg} alt="" />
-                    <h2>Security you can trust</h2>
-                    <p>2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.</p>
-                </Item>
-                <Item>
-                    <img src={collaborationImg} alt="" />
-                    <h2>Real-time collaboration</h2>
-                    <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-                </Item>
-                <Item>
-                    <img src={anyFileImg} alt="" />
-                    <h2>Store any type of file</h2>
-                    <p>Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.</p>
-                </Item>
+                {features.map(feature => {
+                    return (
+                        <Item>
+                            <img src={feature.img.src} alt={feature.img.alt} />
+                            <h2>{feature.title}</h2>
+                            <p>{feature.content}</p>
+                        </Item>
+                    )
+                })}
             </Content>
         </Container>
     )
